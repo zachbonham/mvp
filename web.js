2aeba62ca6588cf3975f08740e8728bad07f2ca9
Zach Bonham <zachbonham@gmail.com>
2013-02-08 09:22:30 -0600

    rename app.js -> web.js; new express; add procfile;

---
+++ web.js
@@ -0,0 +1,35 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes');
+
+  //, http = require('http');
+//
+var app = express();
+
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.static(__dirname + '/public'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+app.get('/', routes.index);
+
+var port = process.env.PORT || 3000;
+app.listen(port);
+
+//http.createServer(app).listen(port);
+
+console.log("Express server listening on port " + port);
